76939668ef7708317e67cf297bcc119499249107
David Shim <76410372+davidshxm@users.noreply.github.com>
2026-01-07 11:00:26 -0500

    Deploying on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/nextjs-portfolio',
-  images: { unoptimized: true }
 };
-module.exports = nextConfig;
 
 export default nextConfig;
